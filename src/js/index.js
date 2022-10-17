15de14cc7b39bec6b3ed52ee1d54ac1ecaf90afd
nsm-m <nasma.dev@gmail.com>
2022-10-17 16:04:00 -0700

    content and link added

--- src/js/index.js
+++ src/js/index.js
@@ -2,22 +2,37 @@
 
 
 
+
+
 // const myCarouselElement = document.querySelector('#carouselExampleIndicators')
 // const carousel = new bootstrap.Carousel(myCarouselElement, {
 //     interval: 2000,
 //     wrap: false
 // })
 
-new Splide('.splide', {
-    type: 'loop',
-    height: '9rem',
-    perPage: 2,
-    breakpoints: {
-        640: {
-            height: '6rem',
-        },
-    },
-});
+
 document.addEventListener('DOMContentLoaded', function () {
-    new Splide('.splide').mount();
+    //new Splide('.splide').mount();
+    var splide =  new Splide('.splide', {
+        type: 'loop',
+      //  height: '9rem',
+        perPage: 1,
+        breakpoints: {
+            820: {
+               // height: '6rem',
+                perPage: 1,
+            },
+            1020:{
+                perPage: 2,
+                gap:10,
+            },
+            2400:{
+                perPage: 3,
+                gap: 10,
+            }
+        },
+    });
+
+    splide.mount();
 });
+
